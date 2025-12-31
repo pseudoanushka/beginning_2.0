97d0fad3be6e33bc07fe13c4660b3fe5339d19d9
pseudoanushka <anushkagupta172006@gmail.com>
2025-12-31 23:52:22 +0530

    fourth

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// import jsonfile from "jsonfile";
+// import moment from "moment";
+// import simpleGit from "simple-git";
+
+// const git = simpleGit();
+// const path = "./data.json";
+
+// const START = moment("2025-03-01");
+// const END = moment("2025-12-29");
+
+// async function run() {
+//   let day = START.clone();
+
+//   while (day.isSameOrBefore(END)) {
+//     const date = day.hour(12).minute(0).second(0).format();
+
+//     await jsonfile.writeFile(path, { active_on: date });
+//     await git.add([path]);
+
+//     await git.commit(`Activity ${date}`, {
+//       "--date": date,
+//     });
+
+//     day.add(1, "day");
+//   }
+
+//   await git.push();
+//   console.log("Backfill complete");
+// }
+
+// run().catch(console.error);
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
@@ -5,27 +36,22 @@ import simpleGit from "simple-git";
 const git = simpleGit();
 const path = "./data.json";
 
-const START = moment("2025-03-01");
-const END = moment("2025-12-29");
-
 async function run() {
-  let day = START.clone();
+  const date = moment("2025-12-30")
+    .hour(12)
+    .minute(0)
+    .second(0)
+    .format();
 
-  while (day.isSameOrBefore(END)) {
-    const date = day.hour(12).minute(0).second(0).format();
+  await jsonfile.writeFile(path, { active_on: date });
+  await git.add([path]);
 
-    await jsonfile.writeFile(path, { active_on: date });
-    await git.add([path]);
-
-    await git.commit(`Activity ${date}`, {
-      "--date": date,
-    });
-
-    day.add(1, "day");
-  }
+  await git.commit(`Activity ${date}`, {
+    "--date": date,
+  });
 
   await git.push();
-  console.log("Backfill complete");
+  console.log("Commit for 30 Dec 2025 done");
 }
 
 run().catch(console.error);
