f622c80025a4b2a9389b1bc78de42eaf74bd8019
pseudoanushka <anushkagupta172006@gmail.com>
2026-01-02 23:50:21 +0530

    six

---
+++ 1480.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        result[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] + nums[i];
+        }
+
+        return result;
+    }
+};
