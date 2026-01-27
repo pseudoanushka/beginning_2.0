3aacd5ebf1cd07966d296d26b66f7f305fe7af5c
Anushka <anushkagupta172006@gmail.com>
2026-01-27 23:57:26 +0530

    27 Jan

---
+++ 496.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int> mp;
+        stack<int> st;
+
+        // Step 1: process nums2
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+
+            while(!st.empty() && st.top() <= nums2[i])
+                st.pop();
+
+            mp[nums2[i]] = st.empty() ? -1 : st.top();
+
+            st.push(nums2[i]);
+        }
+
+        // Step 2: build answer for nums1
+        vector<int> ans;
+        for(int x : nums1)
+            ans.push_back(mp[x]);
+
+        return ans;
+    }
+};
